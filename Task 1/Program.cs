60c769eeaf0cd9de3d058be133d39f9428b5b167
Roman Safroshkin <romasafr@gmail.com>
2021-09-17 19:04:09 +0300

    Task 1, 4

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,18 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Task_1
 {
+    /// <summary>
+    /// Задача 1 
+    /// Сафрошкин Роман
+    /// </summary>
+    /// К сожалению, больше пока не успеваю выполнить, улетел в коммандировки. По возращению обязательно сделаю остальные задачи, но сейчас, для сдачи в срок, отправляю минимум для 
+    /// прохода дальше. 
+
     class Program
     {
+
+        static int PairsNum(int[] array, int n)
+        {
+            int PairsCount = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (array[i] % 3 == 0 || array[i + 1] % 3 == 0)
+                {
+                    PairsCount++;
+                }
+            }
+            return PairsCount;
+        }
+
         static void Main(string[] args)
         {
+            const int arrLen = 20;
+            int[] myArray = new int[arrLen];
+            Random random = new Random();
+            int result;
 
+            Console.WriteLine("Эта программа считает пары в массиве, в которых хотя бы одно число делится на три.");
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < arrLen + 1; i++)
+            {
+                myArray[i] = random.Next(-10000, 10000);
+                Console.Write(myArray[i] + "\n");
+            }
+            Console.WriteLine(" ");
 
+            result = PairsNum(myArray, arrLen);
 
+            Console.WriteLine($"Количество пар: {result}");
+
+            Console.ReadKey();
         }
     }
+
 }
---
+++ Task 2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+
+    /// <summary>
+    /// Задача 4
+    /// Сафрошкин Роман
+    /// </summary>
+    class Program
+    {
+
+        struct Account
+        {
+            public string Login;
+            public string Password;
+
+            public void readFile(string filename)
+            {
+                filename = "..\\..\\" + filename;
+                StreamReader data = new StreamReader(filename);
+
+                Login = data.ReadLine();
+
+                Password = data.ReadLine();
+
+                data.Close();
+            }
+
+        }
+
+        static bool logIN(Account check)
+        {
+            if (check.Login == "root" && check.Password == "GeekBrains")
+                return true;
+            else
+                return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в GeekBrains. Эта программа считает ваш логин и пароль из файла");
+            int tryCount = 3;
+
+            string[] fileName = { "data.txt"};
+
+            Account account;
+            account.Login = "";
+            account.Password = "";
+
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("\nЗагружаем файл с вашими данными: " + fileName[i]);
+                account.readFile(fileName[i]);
+
+                Console.Write("\nРезультат авторизации: ");
+
+                if (logIN(account))
+                {
+
+                    break;
+                }
+                else
+                {
+                    tryCount--;
+                    Console.WriteLine("Неверный ввод логина или пароля." +
+                    Environment.NewLine + "У Вас осталось " + tryCount);
+                }
+                i++;
+            } while (tryCount > 0);
+
+            Console.Write("Авторизация успешна!");
+
+            Console.ReadKey();
+        }
+    }
+}
